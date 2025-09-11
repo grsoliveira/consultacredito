191f3399ed0c1e4b16fd5adcd4a5bd7fca44cd2d
Gleibson Oliveira <gleibson.rodrigo>
2025-09-11 17:57:54 -0300

    Initial commit

---
+++ src/main/java/com/olixcorp/consultacreditoapi/ConsultacreditoApiApplication.java
@@ -0,0 +1,13 @@
+package com.olixcorp.consultacreditoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultacreditoApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsultacreditoApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/olixcorp/consultacreditoapi/ConsultacreditoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olixcorp.consultacreditoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultacreditoApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
